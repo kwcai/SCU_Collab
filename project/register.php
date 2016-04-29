729d8403e1db1073ea6a8446ce9d455491b435d0
kwcai <kcai@scu.edu>
2016-04-29 14:01:23 -0700

    adding

---
+++ project/register.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Register for SCUCollab</title>
+	<link rel="stylesheet" href="styles.css" />
+</head>
+
+<body>
+
+	<?php
+		session_start();
+
+		//check if there is active session or not
+
+		if(isset($_SESSION['user'])!="")
+		{
+			header("Location: home.php")
+		}
+		include_once 'svr_config.php';
+
+		if(isset($_POST['reg_button']))
+		{
+			$fname = mysql_real_escape_string($_POST['fname']);
+			$lname = mysql_real_escape_string($_POST['lname']);
+			$email = mysql_real_escape_string($_POST['email']);
+			$pass = mysql_real_escape_string($_POST['pass']);
+
+			if(mysql_query("INSERT INTO user_log(name_first, name_last, email, password) VALUES ('$namefirst', '$namelast', '$email', '$password')"))
+			{
+			?>
+				<script>alert('You have been successfully registered');</script>
+			<?php
+			}
+			else
+			{
+			?>
+				<script>alert('Registration error');</script>
+			<?php
+			}
+		}
+	?>
+
+	<div id="form">
+		<form name = "reg" method = "post">
+			<input type = "text" name = "namefirst" placeholder = "First Name">
+			<input type = "text" name = "namelast" placeholder = "Last Name">
+			<input type = "text" name = "email" placeholder = "Email">
+			<input type = "password" name = "password" placeholder = "Password">
+			<button type="submit" name="btn-reg">Register</button>button>
+		</form>
+	</div>
+
+	<a href="index.php"> Log In </a>
+
+</body>
+
+
