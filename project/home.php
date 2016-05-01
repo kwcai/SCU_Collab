9a4b1fd2deff7977426a86608cccd954df919fe4
kwcai <kcai@scu.edu>
2016-05-01 16:41:56 -0700

    greeting + link to logout

--- project/home.php
+++ project/home.php
@@ -6,6 +6,8 @@
 
 	$result = mysqli_query($db, "SELECT name_first AND name_last FROM user_log WHERE email='$user_check'");
 	$userRow = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+	$current_user=$row['name_first'] . " " . $row['name_last'];
 	
 	if(!isset($_SESSION['user']))
 	{
@@ -22,10 +24,12 @@
 		<link rel="stylesheet" type="text/css" href="styles.css">
 	</head>
 	<body>
+		<h1>Hello, <em><?php echo $current_user;?>!</em></h1>
 		<h1>SCU Collab</h1>
 		<h2>Create Group</h2>
 		<h2>Join Group</h2>
 		<h2>View Groups</h2>
 		<h2>View Calendar</h2>
+		<a href="logout.php" style="font-size:18px">Logout?</a>
 	</body>
 </html>
