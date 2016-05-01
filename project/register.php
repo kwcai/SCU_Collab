c8ce5aef1a32a7edb70d24ca09a4a68f79c2a9ca
kwcai <kcai@scu.edu>
2016-04-30 17:46:05 -0700

    error fix

--- project/register.php
+++ project/register.php
@@ -48,7 +48,7 @@
 			<input type = "text" name = "lname" placeholder = "Last Name">
 			<input type = "text" name = "email" placeholder = "Email">
 			<input type = "password" name = "password" placeholder = "Password">
-			<button type="submit" name="btn-reg">Register</button>button>
+			<button type="submit" name="btn-reg">Register</button>
 		</form>
 	</div>
 
