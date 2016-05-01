1ce564e6fb70633b4dda40b5c2f5e3cf50e4673c
wesleysha <wsha@scu.edu>
2016-04-30 18:20:04 -0700

    added mysqlihome.php

---
+++ project/mysqlihome.php
@@ -0,0 +1,27 @@
+<?php
+	session_start();
+	include_once 'databaseconnect.php';
+	
+	if(!isset($_SESSION['user']))
+	{
+		header("Location: index.php");
+	}
+	$res = $MySQLi_CON->query("SELECT * FROM users WHERE user_id=".$_SESSION['user']);
+	$userRow = $MySQLi_CON->fetch_array($res);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Home Page</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="styles.css">
+	</head>
+	<body>
+		<h1>SCU Collab</h1>
+		<h2>Create Group</h2>
+		<h2>Join Group</h2>
+		<h2>View Groups</h2>
+		<h2>View Calendar</h2>
+	</body>
+</html>
