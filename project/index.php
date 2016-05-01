8b37764469feb2e61635f64df8719fa7c1830183
wesleysha <wsha@scu.edu>
2016-05-01 15:27:57 -0700

    changed body

--- project/index.php
+++ project/index.php
@@ -1,55 +1,53 @@
 <!DOCTYPE html>
-<body>
-	<?php
-		session_start();
-		include_once '../svr_config.php';
-		
-		if(isset($_SESSION['user'])!="")
-		{
-			header("Location: home.php");
-		}
-		
-		if(isset($_POST['myLoginButton']))
-		{
-			$email = mysqli_real_escape_string($_POST['email']);
-			$pass = mysqli_real_escape_string($_POST['pass']);
-			$res = mysqli_query("SELECT * FROM users WHERE email='$email'");
-			$row = mysqli_fetch_array($res);
-			
-			if($row['password']==md5($pass))
-			{
-				$_SESSION['user'] = $row['user_id'];
-				header("Location: home.php");
-			}
-			else
-			{
-				?>
-				<script>alert('Incorrect login');</script>
-				<?php
-			}
-		}
-	?>
-</body>
-
 <html lang="en">
 	<head>
 		<title>Login Page</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="styles.css">
 	</head>
+	
 	<body>
+		<?php
+			session_start();
+			include_once '../svr_config.php';
+			
+			if(isset($_SESSION['user'])!="")
+			{
+				header("Location: home.php");
+			}
+			
+			if(isset($_POST['myLoginButton']))
+			{
+				$email = mysqli_real_escape_string($_POST['email']);
+				$pass = mysqli_real_escape_string($_POST['pass']);
+				$res = mysqli_query("SELECT * FROM users WHERE email='$email'");
+				$row = mysqli_fetch_array($res);
+				
+				if($row['password']==md5($pass))
+				{
+					$_SESSION['user'] = $row['user_id'];
+					header("Location: home.php");
+				}
+				else
+				{
+				?>
+				<script>alert('Incorrect login');</script>
+				<?php
+				}
+			}
+		?>
 		<h1>SCU Collab</h1>
 		
 		<div class = "input">
-			<input type="text" id="username" name="email" placeholder="email">
+			<input type="text" name="email" placeholder="email">
 		</div>
 		
 		<div class = "input">
-			<input type="password" id="password" name="pwd" placeholder="password">
+			<input type="password" name="pwd" placeholder="password">
 		</div>
 		
 		<div class = "login">
-			<button type="submit" id = "loginButton" name="myLoginButton">Login</button>
+			<button type="submit" name="myLoginButton">Login</button>
 		</div>
 		
 		<div class = "register">
