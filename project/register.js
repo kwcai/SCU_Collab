d9bd61adf956b90494b6daf59ca24f3d8cfad017
kwcai <kcai@scu.edu>
2016-05-05 19:51:06 -0700

    register
    registration seems to work properly. tested form validation, creating
    new account, and using an existing email

--- project/register.js
+++ project/register.js
@@ -1,44 +1,73 @@
 $(document).ready(function()
 {
-	$("#registerbtn").click(function()
+	/* form validation rules */
+
+	$("#reg").validate({
+		
+		rules:
+		{
+			f_name: {
+				required: true
+			},
+			l_name: {
+				required: true
+			},
+			password: {
+				required: true
+			},
+			/*
+			password_check: {
+				required: true,
+				equalTo: '#password'
+			}
+			*/
+			email: {
+				required: true
+				//,email: true
+			}
+		},
+
+		messages:
+		{
+			f_name: "Please enter your first name",
+			l_name: "Please enter your last name",
+			password: "Please enter a password",
+			email: "Please enter an email",
+		},
+
+		submitHandler: submitForm
+
+	})
+
+
+	/* form submission */
+
+	function submitForm()
 	{
 
-		var firstname=$("#f_name").val();
-		var lastname=$("#l_name").val();
-		var email=$("#email").val();
-		var password=$("#password").val();
-		var dataString="f_name="+firstname+"&l_name="+lastname+"&email="+email+"&password="+password+"&register=";
+		var data = $("#reg").serialize();
 
-		if($.trim(firstname).length > 0 & $.trim(lastname).length > 0 & $.trim(email).length > 0 & $.trim(password).length > 0)
-		//if(!$.trim(firstname).val() || !$.trim(lastname).val() || !$.trim(email).val() || !$.trim(password).val())
-		{
-			$.ajax({
-				type: "POST",
-				url: "register.php",
-				data: dataString,
-				crossDomain: true,
-				cache: false,
-				beforeSend: function(){ $("#registerbtn").val('Submitting...');},
-				success: function(data){
-					if(data=="success")
-					{
-					alert("Your account has been registered");
-					}
-					else if(data=="exist")
-					{
-					alert("You already have an account with us");
-					}
-					else if(data=="failed")
-					{
-					alert("Something Went wrong");
-					}
+		$.ajax({
+
+			type: "POST",
+			url: "register.php",
+			data: data,
+			success : function(data)
+			{
+				if(data=="success")
+				{
+				alert("Your account has been registered");
 				}
-			});
-		}
-		else
-		{
-			alert("You must fill out all the fields");
-		}	
+				else if(data=="exist")
+				{
+				alert("You already have an account with us");
+				}
+				else if(data=="failed")
+				{
+				alert("Something Went wrong");
+				}
+			}
+		})
 		return false;
-	});
-});
+	}
+});	
