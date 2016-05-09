84064fa314a46385e9d5f08ea6d663ccccb29ea7
kwcai <kcai@scu.edu>
2016-05-08 18:52:46 -0700

    file changes

---
+++ project/index.js
@@ -0,0 +1,58 @@
+$('document').ready(function()
+{
+	/* form validation */
+	$("#login-form").validate({
+		
+		rules:
+		{
+			password: {
+				required: true
+			},
+			username: {
+				required: true
+			},
+		},
+
+		message:
+		{
+			password: "Enter your password",
+			username: "Enter your password",
+		},
+
+		submitHandler: submitForm
+
+	});
+
+	/* submit login form */
+	function submitForm()
+	{
+
+		var data = $("#login-form").serialize();
+
+		$.ajax({
+
+			type: "POST",
+			url: "index.php",
+			data: data,
+			success : function(data)
+			{
+				alert(data);
+
+				if(data=="success")
+				{
+					localStorage.login="true";
+					localStorage.user=$("#username").val();
+					//window.location.href = "home.html";
+					alert("You are now logged in");
+				}
+				else if(data=="failure")
+				{
+					alert("Incorrect username or password");
+				}
+			}
+		})
+		return false;
+	}
+
+
+});
