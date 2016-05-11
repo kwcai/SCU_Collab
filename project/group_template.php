e9bc0cf0776e9684c5c90beb681c05d1dcd73b02
kwcai <kcai@scu.edu>
2016-05-10 21:43:13 -0700

    creating pages

--- project/group_template.php
+++ project/group_template.php
@@ -1,28 +1,59 @@
 <?php
 
-	include "../../svr_config.php";
+	include('../svr_config.php');
 
 	// If statements to choose function
 
 	/* Call when func is createpost */
-	function createPost() {
+	/*function createPost() {
 		
 
 
+	}*/
+
+	if(isset($_GET['function']))
+	{
+		if($_GET['function'] == "posts")
+		{
+			showPosts();
+		}
+		else
+		{
+			showEvents();
+		}
 	}
 
 	/* Call when param is forum */
 	function showPosts() {
+ 
+    global $db;
+    $group_page = "{group_name}";
+    
+    $stmt = $db->query("SELECT `group_id` FROM `groups` WHERE group_name = '$group_page'"); //would replace in template
+    $row = $stmt->fetch_assoc();
+    $g_id = $row['group_id'];
+    
+    echo $g_id;
 
-		if($result = $db->$query("SELECT * FROM posts WHERE group_id = $u_id ORDER by ID DESC LIMIT 10"))
+		if($result = $db->query("SELECT * FROM `posts` WHERE group_id = '$g_id' ORDER by `post_id` DESC LIMIT 10"))
 		{
+
+			echo "<table>";
+
 			while ($row = $result->fetch_assoc())
 			{
-
+				echo "<tr><td>" . $row['title'] . "</tr></td>";
+				echo "<tr><td>" . $row['data'] . "</tr></td>";
+				echo "<tr><td>" . $row['username'] . "</tr></td>";
 			}
-		}
 
-		$result->free();
+			echo "<table>";
+      
+      		$result->free();
+
+		}
+    else
+      die($db->error);
 
 	}
 
