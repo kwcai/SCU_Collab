3cbe445e53b155efbf1a14ec0010adff2347ddae
wesleysha <wsha@scu.edu>
2016-05-10 18:52:19 -0700

    fixes

--- project/groupsearch.php
+++ project/groupsearch.php
@@ -12,7 +12,7 @@ Groups Table
 
 include('../svr_config.php');
 
-$search = $_GET["key"];
+// $search = $_GET["key"];
 
 $query = "SELECT group_name FROM groups WHERE group_name = ?";
 
@@ -36,54 +36,5 @@ if ($stmt = $db->prepare($query))
 }
 
 echo json_encode($array);
-
-/*while($row = mysql_fetch_assoc($query))
-{
-	$array[] = $row['group_name'];
-}*/
-
-/*
-
-if (strlen($query) > 0)
-{	
-	$hint = "";
-	for($i=0; $i<($x->length); $i++)
-	{
-		$y = $x->item($i)->getElementsByTagName('title');
-		$z = $x->item($i)->getElementsByTagName('url');
-		if ($y->item(0)->nodeType == 1)
-		{
-		//find a link matching the search text
-			if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q))
-			{
-				if ($hint == "")
-				{
-					$hint="<a href='" . 
-					$z->item(0)->childNodes->item(0)->nodeValue . 
-					"' target='_blank'>" . 
-					$y->item(0)->childNodes->item(0)->nodeValue . "</a>";
-				}
-				else
-				{
-					$hint = $hint . "<br /><a href='" . 
-					$z->item(0)->childNodes->item(0)->nodeValue . 
-					"' target='_blank'>" . 
-					$y->item(0)->childNodes->item(0)->nodeValue . "</a>";
-				}
-			}
-		}
-	}
-}
-
-if ($hint=="") {
-	$response="no suggestion";
-}
-else
-{
-  $response=$hint;
-}
-
-echo $results;*/
-
 
 ?>
